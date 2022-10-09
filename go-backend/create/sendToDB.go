25bb9bb1e9786c8d0c47d80f8d6f98c53862c74f
Luuk <luuk.volkering@gmail.com>
2022-10-09 20:07:47 +0200

    It ain't perfect but the golang backend works now.

--- go-backend/create/sendToDB.go
+++ go-backend/create/sendToDB.go
@@ -3,22 +3,34 @@ package main
 import (
 	utility "api.hourkeeper.net/utils"
 	"encoding/json"
+	"fmt"
 	"os"
-	"time"
 )
 
-type Rows struct {
-	Hours float64   `json:"hours"`
-	Date  time.Time `json:"date,string"`
+type Hours struct {
+	Date     string
+	Hours    string
+	UserUUID string
 }
 
 func sendToDB(userSub interface{}, body string) string {
-	var sendTo Rows
-
-	json.Unmarshal([]byte(body), &sendTo)
 	dbUrl := os.Getenv("DB_URL")
 	gormDB := utility.GetDB(dbUrl)
 
-	gormDB.Table("hours")
+	var bodyParseVar map[string]interface{}
+	_ = json.Unmarshal([]byte(body), &bodyParseVar)
+
+	inputDate := fmt.Sprintf("%s", bodyParseVar["date"])
+	inputHours := fmt.Sprintf("%s", bodyParseVar["hours"])
+
+	var userUUID map[string]interface{}
+	gormDB.Table("users").Where("auth0sub LIKE ?", userSub).Find(&userUUID)
+
+	HoursVar := Hours{Date: inputDate, Hours: inputHours, UserUUID: fmt.Sprintf("%s", userUUID["uuid"])}
+	err := gormDB.Select("date", "hours", "user_uuid").Create(&HoursVar)
+	if err != nil {
+		print(err)
+	}
+
 	return "Success!"
 }
--- node-backend/update/pack.sh
+++
@@ -1,3 +0,0 @@
-rm main.zip
-yarn install
-zip main.zip -r node_modules/ index.js package.json yarn.lock root.crt
